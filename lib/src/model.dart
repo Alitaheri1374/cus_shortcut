7ad725a81c4b4578f5b954ec740eb607b7f79438
MohamadArsalan Imamverdi <arsalan@imamverdi.ir>
2024-03-11 10:38:42 +0330

    Publish stable release

--- lib/src/model.dart
+++ lib/src/model.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'env.dart';
 export 'package:flutter/services.dart' show LogicalKeyboardKey;
-class ModelShortCuts{
+class ModelShortcut{
   final LogicalKeySet? logicalKeySet;
   final ShortcutEventTypeEnum? shortcutEventTypeEnum;
   final String? shortCutsName;
   final String? title;
   final Color? color;
   final Widget? widget;
-  ModelShortCuts({this.logicalKeySet,this.shortcutEventTypeEnum,this.title,this.shortCutsName,this.color,this.widget});
+  ModelShortcut({this.logicalKeySet,this.shortcutEventTypeEnum,this.title,this.shortCutsName,this.color,this.widget});
 }
